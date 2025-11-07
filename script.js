8b85da7d6841ce09a676baf87964fc80a32e2ff7
PARDEEP 111 <pardeepsingh091209@gmail.com>
2025-11-07 19:25:48 +0530

    builded the basic logic and basic css

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let taskContainer = document.querySelector(".task-bar")
+let input = document.querySelector("#myInput")
+let btn = document.querySelector(".add-btn")
+let arr = []
+
+
+
+function rerender() {
+    taskContainer.innerHTML = ""
+    arr.forEach((element, index) => {
+        let li = document.createElement("li")
+        let delBtn = document.createElement("button")
+        li.textContent = element
+
+        taskContainer.appendChild(li)
+        li.classList.add("tasks")
+        li.appendChild(delBtn)
+        delBtn.classList.add("delbtn")
+delBtn.textContent="Delete"
+        delBtn.addEventListener("click", () => {
+            arr.splice(index, 1)
+            rerender()
+        })
+
+
+    });
+    // taskContainer.innerHTML = ""
+
+}
+btn.addEventListener("click", () => {
+    // input = arr[i]
+    if(input.value.trim()=== "")return
+    arr.push(input.value.trim())
+    input.value = ""
+    rerender()
+})
 
