9791390f930b60121fdd100cf1c34a5965945d29
PARDEEP 111 <pardeepsingh091209@gmail.com>
2025-11-11 18:07:52 +0530

    adding some css

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let taskContainer = document.querySelector(".task-bar")
 let input = document.querySelector("#myInput")
 let btn = document.querySelector(".add-btn")
+let delBtn = document.createElement("button")
+let li = document.createElement("li")
+let completArr = []
 let arr = []
 
 
@@ -8,28 +11,25 @@ let arr = []
 function rerender() {
     taskContainer.innerHTML = ""
     arr.forEach((element, index) => {
-        let li = document.createElement("li")
-        let delBtn = document.createElement("button")
         li.textContent = element
-
         taskContainer.appendChild(li)
         li.classList.add("tasks")
+
         li.appendChild(delBtn)
         delBtn.classList.add("delbtn")
-delBtn.textContent="Delete"
+        delBtn.textContent = "Delete"
+
         delBtn.addEventListener("click", () => {
             arr.splice(index, 1)
             rerender()
         })
 
-
     });
-    // taskContainer.innerHTML = ""
 
 }
+
 btn.addEventListener("click", () => {
-    // input = arr[i]
-    if(input.value.trim()=== "")return
+    if (input.value.trim() === "") return
     arr.push(input.value.trim())
     input.value = ""
     rerender()
